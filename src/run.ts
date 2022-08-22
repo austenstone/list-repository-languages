3e793a719cd462214ab49848c2b077c7f91dbaf2
Austen Stone <austenstone@github.com>
2022-08-22 00:01:18 -0400

    test

--- src/run.ts
+++ src/run.ts
@@ -5,6 +5,7 @@ interface Input {
   token: string;
   owner: string;
   repo: string;
+  codeql: boolean;
 }
 
 export function getInputs(): Input {
@@ -12,7 +13,22 @@ export function getInputs(): Input {
   result.token = core.getInput('github-token');
   result.owner = core.getInput('owner');
   result.repo = core.getInput('repo');
+  result.codeql = core.getBooleanInput('codeql');
   return result;
+}
+
+const codeqlLanguageMapping = {
+	"csharp": "csharp",
+	"c#": "csharp",
+	"cpp": "cpp",
+	"c": "cpp",
+	"c++": "cpp",
+	"go": "go",
+	"java": "java",
+	"javascript": "javascript",
+	"typescript": "javascript",
+	"python": "python",
+	"ruby": "ruby"
 }
 
 const run = async (): Promise<void> => {
@@ -21,8 +37,11 @@ const run = async (): Promise<void> => {
     const octokit: ReturnType<typeof github.getOctokit> = github.getOctokit(input.token);
     const langResponse = await octokit.request(`GET /repos/${input.owner}/${input.repo}/languages`);
     core.debug(JSON.stringify({langResponse}))
-    const keys = Object.keys(langResponse.data);
-    core.setOutput('languages', JSON.stringify(keys));
+    let languages = Object.keys(langResponse.data);
+    if (input.codeql) {
+      languages = languages.filter(l => codeqlLanguageMapping[l]);
+    }
+    core.setOutput('languages', JSON.stringify(languages));
   } catch (error) {
     core.startGroup(error instanceof Error ? error.message : JSON.stringify(error));
     core.info(JSON.stringify(error, null, 2));
