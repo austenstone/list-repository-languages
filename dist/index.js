55c366f4425a9a1247fdcb0a56782f6b6b71274b
Austen Stone <austenstone@github.com>
2022-08-22 00:08:12 -0400

    test

--- dist/index.js
+++ dist/index.js
@@ -12480,7 +12480,8 @@ const run = () => __awaiter(void 0, void 0, void 0, function* () {
         core.debug(JSON.stringify({ langResponse }));
         let languages = Object.keys(langResponse.data);
         if (input.codeql) {
-            languages = languages.filter(l => codeqlLanguageMapping[l]);
+            console.log
+            languages = languages.filter(l => codeqlLanguageMapping[l.toLowerCase()]);
         }
         core.setOutput('languages', JSON.stringify(languages));
     }
